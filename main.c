a8091cd50f5a46d1f289b22e3d0bdab9a9576d04
sroccaserra <sroccaserra@yahoo.com>
2024-07-16 19:49:30 +0200

    r Go back to blank window

--- main.c
+++ main.c
@@ -5,29 +5,16 @@
 #define FPS 60
 
 static const char *title = "raylib [core] example - basic window";
-static char *default_text = "Congrats! You created your first window!";
-
-static const char *spritesheet_filename = "Sprite-0001.png";
-static const Rectangle sprite1 = {0.f, 0.f, 8.f, 8.f};
-
-void draw_text(char *text, int x, int y) {
-    DrawText(text, x, y, 20, LIGHTGRAY);
-}
 
 int main()
 {
     InitWindow(W, H, title);
-    Texture2D spritesheet = LoadTexture(spritesheet_filename);
-    Vector2 pos = {0, 0};
     SetTargetFPS(FPS);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            draw_text(default_text, 190, 200);
-            DrawTextureRec(spritesheet, sprite1, pos, WHITE);
-            pos.x += 1;
         EndDrawing();
     }
 
