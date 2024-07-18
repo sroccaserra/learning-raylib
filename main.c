ef86bdf1f5a88c3eee9e3901ba9ed7c9d35ef12a
sroccaserra <sroccaserra@yahoo.com>
2024-07-18 14:50:54 +0200

    f Add rotating 3D world & draw_quad function

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <math.h>
 
 #include "raylib.h"
+#include "rlgl.h"
 
 #define W 800
 #define H 450
@@ -37,6 +38,8 @@ struct particule {
 };
 
 struct state {
+    Camera3D camera;
+    float phase;
     struct particule particules[NB_PARTICLES];
 };
 
@@ -44,6 +47,16 @@ struct state {
 #define z_phase(phase) (Z_NEAR + (Z_FAR - Z_NEAR)*sin(phase))
 
 void init(struct state *s) {
+    *s = (struct state){0};
+    rlDisableBackfaceCulling();
+    s->camera = (Camera3D){
+        .position = {10.f*sin(s->phase), 10.f*cos(s->phase), 10.f},
+        .target = {0},
+        .up = { 0.0f, 1.0f, 0.0f },
+        .fovy = 45.f,
+        .projection = CAMERA_PERSPECTIVE,
+    };
+
     for (int i = 0; i < NB_PARTICLES; ++i) {
         struct particule *p = &s->particules[i];
         float phase = (2*M_PI*rand())/RAND_MAX;
@@ -63,6 +76,8 @@ int cmp_z(const void *a, const void *b) {
 }
 
 void update(struct state *s) {
+    s->phase -= 0.025;
+    s->camera.position = (Vector3){10.f*sin(s->phase), 10.f, 10.f*cos(s->phase)};
     for (int i = 0; i < NB_PARTICLES; ++i) {
         struct particule *p = &s->particules[i];
         Vector3 *pos = &p->position;
@@ -72,6 +87,11 @@ void update(struct state *s) {
         pos->z = z_phase(phase+M_PI/2);
     }
     qsort(s->particules, NB_PARTICLES, sizeof(struct particule), cmp_z);
+}
+
+void draw_quad(Vector3 a, Vector3 b, Vector3 c, Vector3 d, Color color) {
+    DrawTriangle3D(a, b, c, color);
+    DrawTriangle3D(c, d, a, color);
 }
 
 int main()
@@ -86,15 +106,32 @@ int main()
     {
         BeginDrawing();
             ClearBackground(BLACK);
+
+            BeginMode3D(state.camera);
+                Vector3 pos = {0};
+                DrawCube(pos, 2.0f, 2.0f, 2.0f, RED);
+                //DrawCubeWires(pos, 2.0f, 2.0f, 2.0f, MAROON);
+
+                float r = 3.f;
+                Vector3 a = {0, 0, 0 },
+                        b = {0, r, 0 },
+                        c = {r, r, 0 },
+                        d = {r, 0, 0 };
+                DrawGrid(100, .50f);
+                draw_quad(a, b, c, d, WHITE);
+            EndMode3D();
+
             for (int i = 0; i < NB_PARTICLES; ++i) {
                 struct particule *p = &state.particules[i];
                 Vector3 *pos = &p->position;
                 float z = (*pos).z;
-                float r = A*z + B;
+                float r = (A*z + B);
                 Color color = (z > Z_MID) ? FAR_COLOR : NEAR_COLOR;
                 DrawRectangle((*pos).x, (*pos).y, r, r, color);
             }
+
         EndDrawing();
+
         update(&state);
     }
 
